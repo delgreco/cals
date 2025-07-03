f143a3fa2e13ac34ee7c9f8f0c2456a658901a66
Marcus Del Greco <marcus@mindmined.com>
2025-07-03 15:25:55 +0000

    First draft

---
+++ cals.pl
@@ -0,0 +1,83 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+=head1 Calify
+
+Merge two or more iCal calendars from their URLs and create a unified iCal file.
+
+=cut
+
+use lib qw(
+    local/lib/perl5
+    local/lib/perl5/x86_64-linux-thread-multi
+);
+
+# use LWP::Simple;
+use LWP::UserAgent;
+use Data::ICal;
+use Data::ICal::Entry::Event;
+use Text::vFile;
+use Text::vFile::asData;
+use Data::Dumper;
+use Dotenv -load;
+
+my $ua = LWP::UserAgent->new();
+# $ua->agent("Calify/1.0");
+$ua->timeout(10);
+
+my $airbnb_url = $ENV{AIRBNB_CAL};
+my $vrbo_url   = $ENV{VRBO_CAL};
+
+# file fetch
+my %downloads = (
+    'airbnb.ics' => $airbnb_url,
+    'vrbo.ics'   => $vrbo_url,
+);
+
+for my $file ( keys %downloads ) {
+    my $url = $downloads{$file};
+    my $response = $ua->get($url, ':content_file' => $file);
+
+    if ($response->is_success) {
+        print "Downloaded $url to $file\n";
+    } 
+    else {
+        die "Failed to download $url: " . $response->status_line . "\n";
+    }
+}
+
+open my $fh, 'airbnb.ics' or die "couldn't open ics: $!";
+my $airbnb_data = Text::vFile::asData->new->parse( $fh );
+
+open $fh, 'vrbo.ics' or die "couldn't open ics: $!";
+my $vrbo_data = Text::vFile::asData->new->parse( $fh );
+
+my $calendar = Data::ICal->new();
+
+# merge events
+for my $cal ($airbnb_data, $vrbo_data) {
+    # print "I see cal $cal\n";
+    for my $event (@{$cal->{objects}[0]{objects}}) {
+        # print "I see event $event\n";
+        next unless $event->{type} eq 'VEVENT';
+        my $entry = Data::ICal::Entry::Event->new();
+        $entry->add_properties(
+            summary     => $event->{properties}->{SUMMARY}[0]{value},
+            dtstart     => $event->{properties}->{DTSTART}[0]{value},
+            dtend       => $event->{properties}->{DTEND}[0]{value},
+            description => $event->{properties}->{DESCRIPTION}[0]{value},
+            uid         => $event->{properties}->{UID}[0]{value} || uuid(),
+        );
+        $calendar->add_entry($entry);
+    }
+}
+
+open my $out, '>', './merged_calendar.ics' or die $!;
+print $out $calendar->as_string;
+close $out;
+
+
+
+
---
+++ cpanfile
@@ -0,0 +1,8 @@
+requires 'Dotenv';
+requires 'LWP::Simple';
+requires 'LWP::Protocol::https';
+requires 'Data::ICal';
+requires 'Data::ICal::Entry::Event';
+requires 'Text::vFile';
+requires 'Text::vFile::asData';
+
