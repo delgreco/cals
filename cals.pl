932cb2bb71a1c2f05da9cfd8e8dc1f93e5375054
Marcus Del Greco <marcus@mindmined.com>
2026-02-19 13:51:26 +0000

    Add Bookings.com

--- cals.pl
+++ cals.pl
@@ -3,7 +3,7 @@
 use strict;
 use warnings;
 
-=head1 Calify
+=head1 Cals
 
 Merge two or more iCal calendars from their URLs and create a unified iCal file.
 
@@ -27,20 +27,22 @@ my $ua = LWP::UserAgent->new();
 # $ua->agent("Calify/1.0");
 $ua->timeout(10);
 
-my $airbnb_url = $ENV{AIRBNB_CAL};
-my $vrbo_url   = $ENV{VRBO_CAL};
+my $airbnb_url   = $ENV{AIRBNB_CAL};
+my $vrbo_url     = $ENV{VRBO_CAL};
+my $bookings_url = $ENV{BOOKINGS_CAL};
 
 # file fetch
 my %downloads = (
-    'airbnb.ics' => $airbnb_url,
-    'vrbo.ics'   => $vrbo_url,
+    'airbnb.ics'   => $airbnb_url,
+    'vrbo.ics'     => $vrbo_url,
+    'bookings.ics' => $bookings_url,
 );
 
 for my $file ( keys %downloads ) {
     my $url = $downloads{$file};
     my $response = $ua->get($url, ':content_file' => $file);
 
-    if ($response->is_success) {
+    if ( $response->is_success ) {
         print "Downloaded $url to $file\n";
     } 
     else {
@@ -54,17 +56,29 @@ my $airbnb_data = Text::vFile::asData->new->parse( $fh );
 open $fh, 'vrbo.ics' or die "couldn't open ics: $!";
 my $vrbo_data = Text::vFile::asData->new->parse( $fh );
 
+open $fh, 'bookings.ics' or die "couldn't open ics: $!";
+my $bookings_data = Text::vFile::asData->new->parse( $fh );
+
 my $calendar = Data::ICal->new();
 
 # merge events
-for my $cal ($airbnb_data, $vrbo_data) {
-    # print "I see cal $cal\n";
+my $count = 0; my $name = '';
+for my $cal ($airbnb_data, $vrbo_data, $bookings_data) {
+    $count++;
+    if ( $count == 1 ) {
+        $name = 'AirBnB';
+    }
+    elsif ( $count == 2 ) {
+        $name = 'VRBO';
+    }
+    else {
+        $name = 'Bookings.com';
+    }
     for my $event (@{$cal->{objects}[0]{objects}}) {
-        # print "I see event $event\n";
         next unless $event->{type} eq 'VEVENT';
         my $entry = Data::ICal::Entry::Event->new();
         $entry->add_properties(
-            summary     => $event->{properties}->{SUMMARY}[0]{value},
+            summary     => "$name: " . $event->{properties}->{SUMMARY}[0]{value},
             dtstart     => $event->{properties}->{DTSTART}[0]{value},
             dtend       => $event->{properties}->{DTEND}[0]{value},
             description => $event->{properties}->{DESCRIPTION}[0]{value},
